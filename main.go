f4e46dc144439fad1d3361806f3212935619ce48
Yuan Xuefeng <yuanxuefeng@casachina.com.cn>
2021-11-19 15:02:36 +0800

    update

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/yleoer/go-demo/hello"
+	"github.com/shirou/gopsutil/process"
 )
 
 func main() {
-	fmt.Println(hello.Greet())
+	p, err := process.NewProcess(int32(os.Getpid()))
+	if err != nil {
+		log.Fatalf("failed to get process %s by pid %d", err, os.Getpid())
+	}
+
+	mp, _ := p.MemoryPercent()
+
+	fmt.Println(mp)
 }
